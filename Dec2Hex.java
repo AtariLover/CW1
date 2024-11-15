f7dfcae33be4a752d7bbc42581a78cb377f12985
ATARILOVER54 <ATARILOVER54@GMAIL.COM>
2024-11-15 16:28:02 +0000

    Updated Code slightly for video demo.

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -55,7 +55,7 @@ class Dec2Hex
 
         String hexadecimal = convert_toHexadec();
         
-        System.out.println("The hexadecimal representation is: " + hexadecimal);
+        System.out.println("The Hexadecimal representation is: " + hexadecimal);
 
 
     }

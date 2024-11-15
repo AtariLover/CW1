f699b73985967031d883ec4c02534782b42b70d5
ATARILOVER54 <ATARILOVER54@GMAIL.COM>
2024-11-15 09:50:24 +0000

    Updated code and files

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 class Dec2Hex
 {
     public static int Arg1;
@@ -8,7 +6,8 @@ class Dec2Hex
     {
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
+        int rem;
+	int num;
         num = Arg1;
         String hexadecimal="";
 

4048c10dd9b5f7ad1a2ec0aa107de8467ec8e26e
ATARILOVER54 <ATARILOVER54@GMAIL.COM>
2024-11-15 09:51:35 +0000

    Updated code from some of sonarqubes suggestions

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,6 +5,7 @@ class Dec2Hex
     public static String convert_toHexadec()
     {
 
+
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem;
 	int num;
